9ae83bae2221187578d860b74033bf4efd0244a5
Juan F. Duque <jfduque@gmail.com>
2022-03-20 16:18:02 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-antsy',
-    version='0.0.2',
+    version='0.0.4',
     description="Python package for integrating Antsy in other applications",
     license='MIT',
     author="Juan F. Duque",
