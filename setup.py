64b224551fcc3b2bbf95df4cb8e2a43a0a5d4cd7
Juan F. Duque <jfelipe@grupodyd.com>
2022-04-21 22:31:44 -0500

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-antsy',
-    version='0.1.2',
+    version='0.0.6',
     description="Python package for integrating Antsy in other applications",
     long_description=open("README.md").read().strip(),
     long_description_content_type="text/markdown",
