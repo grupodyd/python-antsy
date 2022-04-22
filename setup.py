de26187a775ec01cfe4f84b6fbf7f33778177657
Juan F. Duque <jfelipe@grupodyd.com>
2022-04-21 20:42:48 -0500

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-antsy',
-    version='0.1.0',
+    version='0.1.1',
     description="Python package for integrating Antsy in other applications",
     long_description=open("README.md").read().strip(),
     long_description_content_type="text/markdown",
